a9f342b27aeb0e574cf761028742a55edcd1980d
Asma Mubeen <amubeen03asma@gmail.com>
2017-08-18 00:47:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,10 +12,10 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 
 function moveRight(){
-    marginLeft += 10;
+    marginLeft += 3;
     img.style.marginLeft = marginLeft + 'px' ;
 }
 
 img.onclick= function(){
-  var interval = setInterval(moveRight, 100);  
+  var interval = setInterval(moveRight, 50);  
 };
