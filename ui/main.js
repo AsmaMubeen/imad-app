e45ead64b3ef94ab08d681cdbb58afdc035aa4b2
Asma Mubeen <amubeen03asma@gmail.com>
2017-08-19 23:30:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,27 @@
-console.log('Loaded!');
+//Counter code
+var button = document.getElementById('counter');
 
-//Change the text of the main-text div
 
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'New value'; 
-
-//move the image
-var img = document.getElementById('madi');
-
-var marginLeft = 0;
-
-function moveRight(){
-    marginLeft += 3;
-    img.style.marginLeft = marginLeft + 'px' ;
-}
-
-img.onclick= function(){
-  var interval = setInterval(moveRight, 50);  
+button.onclick=  function(){
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the request and store in a variable
+    
+    request.onreadystatechange = function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            //Take some action
+            if(request.Status == 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //Not done yet
+        
+    };
+    
+    //Make the request
+    request.open('GET','http://amubeen03asma.imad.hasura-app.io/counter',true);
+    res.send(null);
 };
