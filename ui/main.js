a274fb5eef46102cc94a41aa7deb162832e947c6
Asma Mubeen <amubeen03asma@gmail.com>
2017-08-24 01:06:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ button.onclick=  function(){
 };
 
 //Submit name
-var nameInput = document.getElementbyId('name');
+var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
@@ -38,8 +38,8 @@ submit.onclick = function(){
     
     //Capture a list of names and render it as a list
     var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0;i< names.length;i++)
+    var list = ' ';
+    for(var i=0;i < names.length;i++)
     {
         list += '<li>'+ names[i] + '</li>';
     }
